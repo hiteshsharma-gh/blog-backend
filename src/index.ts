5e1bee7d95b3730b03f3959e547a9e50d3b011f0
Hitesh Sharma <hiteshsharma.2314@gmail.com>
2024-04-23 21:56:10 +0530

    initialize zod

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,14 @@
 import { Hono } from 'hono'
+import userHandler from './handlers/userHander'
+import blogHandler from './handlers/blogHandler'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string
+  }
+}>()
 
-app.get('/api/v1/blog/:blogId', (c) => {
-  const pageId = c.req.param('blogId')
-  return c.json({
-    page: "blog",
-    pageId,
-    method: "get"
-  })
-})
-
-app.post('/api/v1/user/signup', (c) => {
-  return c.json({
-    page: "user/signup",
-    method: "post"
-  })
-})
-
-app.post('/api/v1/user/signin', (c) => {
-  return c.json({
-    page: "user/signin",
-    method: "post"
-  })
-})
-
-app.post('/api/v1/blog', (c) => {
-  return c.json({
-    page: "blog",
-    method: "post"
-  })
-})
-
-app.put('/api/v1/blog', (c) => {
-  return c.json({
-    page: "blog",
-    method: "put"
-  })
-})
+app.route('/api/v1/user', userHandler)
+app.route('/api/v1/blog', blogHandler)
 
 export default app
