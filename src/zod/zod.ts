6c1404cf65e4c1e1604ebe61fba0312635544b16
Hitesh Sharma <hiteshsharma.2314@gmail.com>
2024-04-24 21:37:25 +0530

    add singup and singin validation

--- src/zod/zod.ts
+++ src/zod/zod.ts
@@ -1,3 +1,12 @@
 import { z } from "zod"
 
-const userSchema = 
+export const singupSchema = z.object({
+  username: z.string().max(20),
+  email: z.string().email(),
+  password: z.string().min(6)
+})
+
+export const signinSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6)
+})
