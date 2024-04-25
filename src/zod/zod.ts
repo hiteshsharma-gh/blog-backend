c6c046c5dedbfd040d61014c0cdf037d2c3238a6
Hitesh Sharma <hiteshsharma.2314@gmail.com>
2024-04-25 22:05:50 +0530

    add postSchema

--- src/zod/zod.ts
+++ src/zod/zod.ts
@@ -10,3 +10,8 @@ export const signinSchema = z.object({
   email: z.string().email(),
   password: z.string().min(6)
 })
+
+export const postSchema = z.object({
+  title: z.string(),
+  content: z.string()
+})
