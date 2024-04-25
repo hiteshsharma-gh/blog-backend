d96d6c29ce553dbde72e36eb35beca69c6c3ba4a
Hitesh Sharma <hiteshsharma.2314@gmail.com>
2024-04-25 22:06:59 +0530

    add middleware

--- src/handlers/blogHandler.ts
+++ src/handlers/blogHandler.ts
@@ -1,13 +1,41 @@
+import { zValidator } from "@hono/zod-validator"
 import { Hono } from "hono"
+import { verify } from "hono/jwt"
+import { postSchema } from "../zod/zod"
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string,
     JWT_SECRET: string
+  },
+  Variables: {
+    userId: string
   }
 }>()
 
-app.get('/api/v1/blog/:blogId', (c) => {
+app.use('/*', async (c, next) => {
+  const jwt = c.req.header("Authorization")
+  if (!jwt) {
+    c.status(401)
+    return c.json({
+      error: "jwt not found"
+    })
+  }
+
+  const payload = await verify(jwt, c.env.JWT_SECRET)
+  if (!payload) {
+    c.status(401)
+    return c.json({
+      error: "unauthorized"
+    })
+  }
+
+  c.set("userId", payload.id)
+
+  await next()
+})
+
+app.get('/:blogId', (c) => {
   const pageId = c.req.param('blogId')
   return c.json({
     page: "blog",
@@ -16,14 +44,21 @@ app.get('/api/v1/blog/:blogId', (c) => {
   })
 })
 
-app.post('/api/v1/blog', (c) => {
-  return c.json({
-    page: "blog",
-    method: "post"
-  })
+app.post('/', zValidator('json', postSchema, (result: any, c) => {
+  if (!result.success) {
+    c.status(403)
+    return c.json({
+      error: "invalid post inputs"
+    })
+  }
+
+  return result.data
+}), async (c) => {
+  const { title, content } = c.req.valid('json')
+
 })
 
-app.put('/api/v1/blog', (c) => {
+app.put('/', (c) => {
   return c.json({
     page: "blog",
     method: "put"
